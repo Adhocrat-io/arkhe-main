c307f5be44a5c13581bf113343eaa770a79e9586
Luc Delaborde <lucdelaborde@gmail.com>
2025-10-09 13:35:34 +0200

    Initializes core user management module
    Sets up the foundational structure for user, permission, and role management.
    
    This commit initializes the composer package, defining dependencies like Laravel framework, webfonts, SEO tools, login link functionality, permission management, and cookie consent.
    
    It also includes a basic service provider for future functionality registration.

---
+++ src/ArkheMainServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arkhe\Main;
+
+use Illuminate\Support\ServiceProvider;
+
+class ArkheMainServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}

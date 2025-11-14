edb597696033afe42ae97cd06628d4b2bc25c4c1
Luc Delaborde <lucdelaborde@gmail.com>
2025-11-14 19:45:25 +0100

    Remove an old namespace reference

--- stubs/database/seeders/DatabaseSeeder.php
+++ stubs/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Arkhe\Blog\Database\Seeders\RolesAndPermissionsSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder

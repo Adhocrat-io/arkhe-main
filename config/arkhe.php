cc5f15a192d812ebb228fa77f97d1ffdc9a2d194
Luc Delaborde <lucdelaborde@gmail.com>
2025-11-17 14:23:40 +0100

    Adds Arkhe configuration file.
    Creates a configuration file to manage admin settings, permissions, and roles.
    
    This file defines the admin prefix, allowed roles, and granular permissions for different user roles within the application, enhancing security and access control.

--- config/arkhe.php
+++ config/arkhe.php
@@ -15,4 +15,58 @@ return [
             UserRoleEnum::CONTRIBUTOR->value,
         ],
     ],
+    'permissions' => [
+        'manage-users' => [
+            'view-user',
+            'create-user',
+            'update-user',
+            'delete-user',
+        ],
+        'manage-roles' => [
+            'view-role',
+            'create-role',
+            'update-role',
+            'delete-role',
+        ],
+        'manage-customizations' => [
+            'view-customization',
+            'create-customization',
+            'update-customization',
+            'delete-customization',
+        ],
+        'manage-subscriptions' => [
+            'view-subscription',
+            'create-subscription',
+            'update-subscription',
+            'delete-subscription',
+        ],
+        'manage-settings' => [
+            'view-setting',
+            'update-setting',
+            'delete-setting',
+        ],
+    ],
+    'roles' => [
+        UserRoleEnum::ROOT->value => [
+            '*'
+        ],
+        UserRoleEnum::ADMIN->value => [
+            '*'
+        ],
+        UserRoleEnum::AUTHOR->value => [
+            'arkhe.permissions.posts.*',
+        ],
+        UserRoleEnum::CONTRIBUTOR->value => [
+            'arkhe.permissions.posts.*',
+        ],
+        UserRoleEnum::EDITORIAL->value => [
+            'arkhe.permissions.posts.*',
+        ],
+        UserRoleEnum::SUBSCRIBER->value => [
+            'arkhe.permissions.subscriptions.*',
+        ],
+        UserRoleEnum::GUEST->value => [
+            'arkhe.permissions.settings.*',
+        ],
+    ]
 ];

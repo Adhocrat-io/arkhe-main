335a5f35b6a8e97a0be9fee6871c052dab1c2f4c
Luc Delaborde <lucdelaborde@gmail.com>
2026-01-22 13:25:17 +0100

    Adds states to the UserFactory

--- tests/Fixtures/UserFactory.php
+++ tests/Fixtures/UserFactory.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Database\Factories;
 
 use App\Models\User;
+use Arkhe\Main\Enums\Users\UserRoleEnum;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -35,10 +36,61 @@ class UserFactory extends Factory
         ];
     }
 
+    public function configure(): static
+    {
+        return $this->afterCreating(function ($user) {
+            if ($user->roles()->count() === 0) {
+                $user->assignRole(UserRoleEnum::SUBSCRIBER->value);
+            }
+        });
+    }
+
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
+    }
+
+    public function withRole(UserRoleEnum $role): static
+    {
+        return $this->afterCreating(function ($user) use ($role) {
+            $user->syncRoles([$role->value]);
+        });
+    }
+
+    public function root(): static
+    {
+        return $this->withRole(UserRoleEnum::ROOT);
+    }
+
+    public function admin(): static
+    {
+        return $this->withRole(UserRoleEnum::ADMIN);
+    }
+
+    public function editorial(): static
+    {
+        return $this->withRole(UserRoleEnum::EDITORIAL);
+    }
+
+    public function author(): static
+    {
+        return $this->withRole(UserRoleEnum::AUTHOR);
+    }
+
+    public function contributor(): static
+    {
+        return $this->withRole(UserRoleEnum::CONTRIBUTOR);
+    }
+
+    public function subscriber(): static
+    {
+        return $this->withRole(UserRoleEnum::SUBSCRIBER);
+    }
+
+    public function guest(): static
+    {
+        return $this->withRole(UserRoleEnum::GUEST);
     }
 }
